31ed62605898f177ffb87acf5bbd541e6a9096af
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-21 10:56:09 -0800

    chore: create missing 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Home, Missing } from "pages";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello Notebook!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Missing/Missing.tsx
@@ -0,0 +1,15 @@
+import "./Missing.css";
+
+export const Missing = () => {
+  return (
+    <div className="errordiv">
+      <h1>
+        <span className="firstQuestion">?</span>?
+        <span className="secondQuestion">?</span>
+      </h1>
+      <h2>404 - Page is missing</h2>
+      <p>The page you are looking for is missing or doesn't exist.</p>
+      <a href="/">Please click here to return to the homepage</a>
+    </div>
+  );
+};
