1c8e3f4766c37f9e0884fcdb0cd4065d2b54edfd
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-24 18:05:48 -0800

    chore: update sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,6 +4,8 @@ export const Sidebar = () => {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
+      <p>Enter sidebar here</p>
+      <p>But make it actually a sidebar</p>
     </div>
   );
 };
