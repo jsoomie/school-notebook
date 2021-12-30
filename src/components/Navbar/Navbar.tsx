dcc0181d95ec1a3ee9a920d2882cdc8c5ba8ccf5
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-30 07:42:22 -0800

    chore: create navbar with material ui components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,38 @@
 import { Link } from "react-router-dom";
-import { Typography, List, ListItem } from "@mui/material";
+import {
+  Typography,
+  List,
+  ListItem,
+  Box,
+  AppBar,
+  Toolbar,
+} from "@mui/material";
 import "./Navbar.css";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar">
-      <List>
-        <ListItem className="logo">
-          <Link to="/">
-            <Typography variant="h3">School Notebook</Typography>
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/">
-            <Typography variant="body1">Home</Typography>
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/sections">
-            <Typography variant="body1">Sections</Typography>
-          </Link>
-        </ListItem>
-      </List>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky" className="navbar">
+        <Toolbar>
+          <List>
+            <ListItem className="logo">
+              <Link to="/">
+                <Typography variant="h3">School Notebook</Typography>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/">
+                <Typography variant="body1">Home</Typography>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/sections">
+                <Typography variant="body1">Sections</Typography>
+              </Link>
+            </ListItem>
+          </List>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
