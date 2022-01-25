2acfe3aad3ee7d5379e81a18c6a7f43a21e8bb44
Jonathan Soukaseume <sksmejn@gmail.com>
2022-01-25 07:25:59 -0800

    chore: add english time

--- src/pages/English/English.tsx
+++ src/pages/English/English.tsx
@@ -4,6 +4,10 @@ export const English = () => {
   return (
     <div>
       <h1>English</h1>
+      <p>Class Started 1/24/2022</p>
+      <p>
+        Meetings are only on Mondays and Wednesdays are reserved for working.
+      </p>
     </div>
   );
 };
