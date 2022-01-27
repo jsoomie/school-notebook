b8c475208d222a01e8d168168f4885eeec55a78e
Jonathan Soukaseume <sksmejn@gmail.com>
2022-01-26 16:55:58 -0800

    chore: add math

--- src/pages/Math/Math.tsx
+++ src/pages/Math/Math.tsx
@@ -4,6 +4,8 @@ export const Math = () => {
   return (
     <div>
       <h1>Math</h1>
+      <p>Wednesday class @ 1245 - 1445</p>
+      <p>Only meet on exam days besides the first 2 classes</p>
     </div>
   );
 };
