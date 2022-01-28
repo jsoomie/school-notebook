92144bc92d3f7bd4a97cf75d6f8822b7d60968cb
Jonathan Soukaseume <sksmejn@gmail.com>
2022-01-27 20:17:54 -0800

    chore: add math hw

--- src/pages/Math/Math.tsx
+++ src/pages/Math/Math.tsx
@@ -6,6 +6,7 @@ export const Math = () => {
       <h1>Math</h1>
       <p>Wednesday class @ 1245 - 1445</p>
       <p>Only meet on exam days besides the first 2 classes</p>
+      <p>Homework given per week. Must do hw and eDLAs as done per module</p>
     </div>
   );
 };
