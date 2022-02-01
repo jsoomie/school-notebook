37d904414f665e7f32a5ac50b6de525bf35e68af
Jonathan Soukaseume <sksmejn@gmail.com>
2022-02-01 09:39:09 -0800

    chore: add english works

--- src/pages/English/English.tsx
+++ src/pages/English/English.tsx
@@ -9,6 +9,10 @@ export const English = () => {
         Meetings are only on Mondays and Wednesdays are reserved for working.
       </p>
       <p>Find article to write rhetorical paper on. Due Feb. 1 2359</p>
+      <p>
+        Write about one article and do the same thing to another article after
+        reading from part B
+      </p>
     </div>
   );
 };
