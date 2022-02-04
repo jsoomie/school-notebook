d999b79dc2454748233ad5b2c9f986167127750f
Jonathan Soukaseume <sksmejn@gmail.com>
2022-02-04 13:39:18 -0800

    chore: update art xd hw

--- src/pages/Art/Art.tsx
+++ src/pages/Art/Art.tsx
@@ -6,6 +6,9 @@ export const Art = () => {
     <Box>
       <Typography variant="h1">Art</Typography>
       <Typography variant="body1">Classes are TTH @ 1900-2130</Typography>
+      <Typography variant="body2">
+        Finish watching video online and continue with classwork and finish hw
+      </Typography>
     </Box>
   );
 };
