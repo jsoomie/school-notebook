c4850b2883010b5480eada487a845246d106085e
Jonathan Soukaseume <sksmejn@gmail.com>
2022-04-30 16:12:16 -0700

    chore: update psy

--- src/pages/Psy/Psy.tsx
+++ src/pages/Psy/Psy.tsx
@@ -18,6 +18,7 @@ export const Psy = () => {
       <p>Class was canceled last week</p>
       <p>Psy today, quiz due this sunday</p>
       <p>Study for quiz</p>
+      <p>Study now</p>
     </div>
   );
 };
