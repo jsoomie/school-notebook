5c98170f7210f501ad97d0c6cfe80e1d5889f47c
Jonathan Soukaseume <sksmejn@gmail.com>
2022-05-18 08:15:22 -0700

    chore: update missing added a new line inside

--- src/pages/Missing/Missing.tsx
+++ src/pages/Missing/Missing.tsx
@@ -10,6 +10,7 @@ export const Missing = () => {
       <h2>404 - Page is missing</h2>
       <p>The page you are looking for is missing or doesn't exist.</p>
       <a href="/">Please click here to return to the homepage</a>
+      <p>Everything is missing!</p>
     </div>
   );
 };
